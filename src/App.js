fd3f50edfe16999a50c03d0450f9cbfe58b5333d
И Марк <imark.work.ac@gmail.com>
2021-08-23 19:19:08 +0300

    import React

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Typography } from 'antd';
@@ -10,9 +11,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <Button type="dashed">
           <Text type="danger">React</Text>
         </Button>
